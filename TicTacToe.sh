5b58bbb53ca8409cdd917d62ac6b2463ac83ca17
Bhushan Saundale <saundalebhushan5@gmail.com>
2020-03-26 12:03:20 +0530

    [BHUSHAN]Added:Start fresh with resetting the board code.

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,2 +1,32 @@
 #!/bin/bash
      echo "Welcome to Tic Tac Toe Game"
+
+   row=3
+column=3
+
+declare -A game_Board
+function resettingBoard()
+{
+    for ((i=0;i<row;i++))
+    do
+      for((j=0;j<column;j++))
+      do  game_Board[$i,$j]="-"
+      done
+    done
+}
+
+function displayGameBoard() {
+	echo "               "
+	for((i=0; i<row; i++))
+	do
+		for((j=0; j<column; j++))
+		do
+			echo -n " ${game_Board[$i,$j]} |"
+		done
+		echo
+		echo "               "
+	done
+}
+
+resettingBoard
+displayGameBoard
